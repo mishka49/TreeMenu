e47b9d0fae45292646f4fc253668932740fc67e8
mishka49 <andreym3ch@gmail.com>
2024-04-17 17:03:25 +0300

    create recursion template menu

--- menu/templatetags/common_tags.py
+++ menu/templatetags/common_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from menu.models import MenuItem
+from menu.services import menu_to_dict
 
 register = template.Library()
 
@@ -7,6 +8,8 @@ register = template.Library()
 @register.inclusion_tag('menu/menu.html')
 def draw_menu(menu_name):
     menu_items = MenuItem.objects.filter(menu__name=menu_name)
+    tree_menu_items = menu_to_dict(menu_items)
+    print(f"TREE MENU: {tree_menu_items}")
     return {
-        "menu_items": menu_items,
+        "menu_items": tree_menu_items,
     }
